5206397d0aeac208cd74c6f6f5523754fbc49a87
young-geun <dudrmshub@gmail.com>
2019-03-20 00:07:13 +0900

    ggplot default color

---
+++ R/gg_color.R
@@ -0,0 +1,13 @@
+#' Default ggplot color
+#'
+#' @description ggplot default colors
+#' @param n group numbers
+#' @example
+#' gg_hcl(3)
+#' @return gives pallete values
+#' @export
+gg_hcl <- function(n) {
+  hue <- seq(15, 375, length.out = n + 1)
+  hcl_cols <- hcl(h = hue, c = 100, l = 65)[-(n + 1)]
+  hcl_cols
+}
